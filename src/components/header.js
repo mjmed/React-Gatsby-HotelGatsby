f46aa6e3c581e7e1ab3c6d7bee83aec35e654286
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-10-20 10:16:13 -0300

    Primeros pasos con Gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,42 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import React from 'react';
+import { css } from '@emotion/react';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+import Navegacion from './nav';
+
+
+const Header = () => {
+    return (
+        <header
+            css={css`
+                background-color: #222;
+                padding: 1rem;
+            `}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+            <div
+                css={css`
+                    max-width: 1200px;
+                    margin: 0 auto;
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+                    @media (min-width: 768px) {
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                    }
+                `}
+            >
+                <h1
+                    css={css`
+                        color: #FFF;
+                        text-align: center;
+                    `}
+                >
+                    Hotel Gatsby
+                </h1>
+
+                <Navegacion />
+            </div>
+        </header>
+    )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,55 +1,53 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react';
+import Helmet from 'react-helmet';
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { Global, css } from '@emotion/react';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+const Layout = ( props ) => {
+    return (
+        <>
+            <Global 
+                styles={css`
+                    html {
+                        font-size: 62.5%;
+                    }
+                    body {
+                        font-size: 18px;
+                        font-size: 1.8rem;
+                        line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    h1, h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto', serif;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                `}
+            />
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
+            </Helmet>
+            
+            <Header />
+            
+            { props.children }
+        </>
+    )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
